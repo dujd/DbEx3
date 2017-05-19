533fbe303fb5bdc8a5adf210017a7701b46ed268
Даниел Кръстев <daniel.krastev@outlook.com>
2017-05-19 13:21:47 +0300

    Initial DbEx3 commit.

---
+++ DbEx3/DbEx3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DbEx3.MvcApplication" Language="C#" %>
---
+++ DbEx3/DbEx3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(DbEx3.Startup))]
+namespace DbEx3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
